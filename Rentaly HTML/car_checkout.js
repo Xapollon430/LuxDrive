9a2007a5ecb404441af1ab6cea4576770f0d932c
Xapollon430 <Xapollon430@gmail.com>
2023-09-13 16:41:41 -0400

    123

---
+++ Rentaly HTML/car_checkout.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Get a reference to the form, the submit button, and the car name element
+  const contactForm = document.getElementById("contact_form");
+  const submitButton = document.getElementById("send_message");
+  const carNameElement = document.getElementById("car_name"); // Add this line
+
+  // Add an event listener to the form for the submit event
+  contactForm.addEventListener("submit", function (e) {
+    e.preventDefault(); // Prevent the default form submission
+
+    // Collect the form data
+    const formData = new FormData(contactForm);
+
+    // Create an object to store the form data
+    const formDataObject = {};
+    formData.forEach((value, key) => {
+      formDataObject[key] = value;
+    });
+
+    // Additional fields (Name, Email, Phone, Car Name)
+    formDataObject.Name = document.querySelector('input[name="Name"]').value;
+    formDataObject.Email = document.querySelector('input[name="Email"]').value;
+    formDataObject.Phone = document.querySelector('input[name="Phone"]').value;
+    formDataObject.CarName = carNameElement.textContent; // Add this line
+
+    // Send a POST request to localhost:3000
+    fetch("http://localhost:3000/send-dropoff-email", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json", // Set the content type to JSON
+      },
+      body: JSON.stringify(formDataObject), // Convert form data to JSON
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // Handle the response here (e.g., show success or error message)
+        console.log(data);
+        // You can add more code here to handle the response as needed
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  });
+});
---
+++ server.js
@@ -0,0 +1,90 @@
+const express = require("express");
+const nodemailer = require("nodemailer");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const port = 3000;
+
+// Middleware to parse JSON data from the request body
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+// POST endpoint to send an email
+app.post("/send-email", async (req, res) => {
+  console.log(req.body);
+  try {
+    // Extract data from the request body
+    const { Name, Email, phone, message } = req.body;
+
+    // Create a transporter object using SMTP transport
+    const transporter = nodemailer.createTransport({
+      service: "Gmail",
+      auth: {
+        user: "Xapollon430@gmail.com",
+        pass: process.env.PASSWORD,
+      },
+    });
+
+    // Email data
+    const mailOptions = {
+      from: "Xapollon430@gmail.com",
+      to: "drivewithlux@gmail.com", // Recipient's email address
+      subject: "Question From Customer",
+      text: `Customer with the name of ${Name}, email of ${Email}, and number of ${phone} left this message: "${message}"`,
+    };
+
+    // Send the email
+    await transporter.sendMail(mailOptions);
+
+    res.status(200).json({ message: "Email sent successfully" });
+  } catch (error) {
+    console.error("Error sending email:", error);
+    res.status(500).json({ error: "Error sending email" });
+  }
+});
+
+// New endpoint to send an email with drop-off location, date, time, and car name
+app.post("/send-dropoff-email", async (req, res) => {
+  console.log(req.body);
+  try {
+    // Extract data from the request body
+    const Name = req.body.Name;
+    const Email = req.body.Email;
+    const Phone = req.body.Phone;
+    const DropoffLocation = req.body.DropoffLocation;
+    const PickUpDate = req.body["Pick Up Date"];
+    const CollectionDate = req.body["Collection Date"];
+    const CarName = req.body.CarName; // Add this line to extract car name
+
+    // Create a transporter object using SMTP transport
+    const transporter = nodemailer.createTransport({
+      service: "Gmail",
+      auth: {
+        user: "Xapollon430@gmail.com",
+        pass: process.env.PASSWORD,
+      },
+    });
+
+    // Email data
+    const mailOptions = {
+      from: "Xapollon430@gmail.com",
+      to: "drivewithlux@gmail.com", // Recipient's email address
+      subject: "Drop-off Information",
+      text: `Customer with the name of ${Name}, email of ${Email}, and number of ${Phone} has the following drop-off details for car "${CarName}":\n\nDrop-off Location: ${DropoffLocation}\nDrop-off Date: ${PickUpDate}\nDrop-off Time: ${CollectionDate}\n\n`,
+    };
+
+    // Send the email
+    await transporter.sendMail(mailOptions);
+
+    res.status(200).json({ message: "Drop-off email sent successfully" });
+  } catch (error) {
+    console.error("Error sending drop-off email:", error);
+    res.status(500).json({ error: "Error sending drop-off email" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
